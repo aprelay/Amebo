1c538d34a2208aa6252ad80230d9ad5bcdd37056
aprelay <ap@ac-payable.com>
2025-12-21 18:29:53 +0000

    fix: Force cache refresh with new version number
    Changed version from 20251221-notif-dropdown to 20251221-final-fix
    This forces browsers to download the latest fixed JavaScript file
    instead of using cached old version with bugs.

--- dist/_worker.js
+++ dist/_worker.js
@@ -653,8 +653,8 @@ var Ir=Object.defineProperty;var We=e=>{throw TypeError(e)};var Sr=(e,r,t)=>r in
         <div id="app"></div>
         
         <!-- V3 INDUSTRIAL GRADE - E2E Encryption + Token System + Enhanced Features -->
-        <script src="/static/crypto-v2.js?v=20251221-notif-dropdown"><\/script>
-        <script src="/static/app-v3.js?v=20251221-notif-dropdown"><\/script>
+        <script src="/static/crypto-v2.js?v=20251221-final-fix"><\/script>
+        <script src="/static/app-v3.js?v=20251221-final-fix"><\/script>
         
         <script>
           // Register service worker for PWA
